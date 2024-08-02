257a6de8282faac0fd8227bf89bcae58598667f9
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2024-08-02 16:02:01 +0200

    git init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "diplomatico"
+include(":demo:app")
