67655b827ea2b6a1a5e354d594f78bea96417be8
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2024-08-28 19:11:58 +0200

    implement UpdateRoomLevelsTask

--- gradle/plugins/src/main/kotlin/com/alecarnevale/diplomatico/gradle/UpdateRoomLevelsTask.kt
+++ gradle/plugins/src/main/kotlin/com/alecarnevale/diplomatico/gradle/UpdateRoomLevelsTask.kt
@@ -9,6 +9,11 @@ import org.gradle.api.tasks.TaskAction
 internal abstract class UpdateRoomLevelsTask : DefaultTask() {
   @TaskAction
   fun check() {
-    TODO("To do")
+    // TODO: release/dubug to be taken as input
+    val buildReport = project.layout.buildDirectory.file("generated/ksp/debug/resources/com/alecarnevale/diplomatico/results/report.csv")
+    // TODO: path of report file tracked with git could be optionally set in input
+    val assetReport = project.layout.projectDirectory.file("src/main/assets/diplomatico/report.csv")
+
+    assetReport.asFile.writeText(buildReport.get().asFile.readText())
   }
 }
