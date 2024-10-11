f364d8f80fcfcdf789815c6bfee170d04c2a6637
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2024-10-11 18:15:21 +0200

    add test case for one level of nesting

---
+++ processors/src/test/kotlin/com/alecarnevale/diplomatico/HashingRoomDBVersionProcessorProviderNestingClassesTest.kt
@@ -0,0 +1,95 @@
+package com.alecarnevale.diplomatico
+
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+// Test suite for specific to cover nesting classes case
+@OptIn(ExperimentalCompilerApi::class)
+internal class HashingRoomDBVersionProcessorProviderNestingClassesTest {
+  @Test
+  fun `GIVEN a class FooParent with a nested data class FooChild and its FooDatabase, WHEN @HashingRoomDBVersion is applied to FooDatabase and a property of FooChild changes, THEN hashing value in the report changes`() {
+    var fooChild =
+      SourceFile.kotlin(
+        "FooChild.kt",
+        """
+        package com.example
+
+        data class FooChild(
+          val x: Int,
+        )
+        """.trimIndent(),
+      )
+
+    val fooParent =
+      SourceFile.kotlin(
+        "FooParent.kt",
+        """
+        package com.example
+
+        import androidx.room.Entity
+        
+        @Entity
+        data class FooParent(
+          val x: FooChild,
+        )
+        """.trimIndent(),
+      )
+
+    val fooDatabase =
+      SourceFile.kotlin(
+        "FooDatabase.kt",
+        """
+        package com.example
+
+        import androidx.room.Database
+        import com.alecarnevale.diplomatico.api.HashingRoomDBVersion
+        
+        @HashingRoomDBVersion
+        @Database(entities = [FooParent::class])
+        class FooDatabase
+        """.trimIndent(),
+      )
+
+    var result = compileSourceFiles(fooChild, fooParent, fooDatabase)
+
+    assertEquals(KotlinCompilation.ExitCode.OK, result.result.exitCode)
+
+    result.assertGeneratedResources("com/alecarnevale/diplomatico/results/report.csv")
+    result.assertGeneratedContent(
+      "com/alecarnevale/diplomatico/results/report.csv",
+      """
+      com.example.FooDatabase,VXV8r9+A5Dah0CFH58mqUmgOPWN1GDrCeTNcglifBEs=
+      
+      """,
+    )
+
+    fooChild =
+      SourceFile.kotlin(
+        "FooChild.kt",
+        """
+        package com.example
+
+        data class FooChild(
+          val x: Int,
+          val y: String,
+        )
+        """.trimIndent(),
+      )
+
+    result = compileSourceFiles(fooChild, fooParent, fooDatabase)
+
+    assertEquals(KotlinCompilation.ExitCode.OK, result.result.exitCode)
+
+    result.assertGeneratedResources("com/alecarnevale/diplomatico/results/report.csv")
+    result.assertGeneratedContent(
+      "com/alecarnevale/diplomatico/results/report.csv",
+      """
+      com.example.FooDatabase,H5kWB5LSqBIklkIObUliZiz8xff0dU9ZG/ZZcIi05tU=
+      
+      """,
+    )
+  }
+}
