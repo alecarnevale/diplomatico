816aa3bddcb1e7c66ea046f113c7dd9936dccec1
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2024-10-11 19:37:54 +0200

    add kotlin serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlinter) apply false
 }
