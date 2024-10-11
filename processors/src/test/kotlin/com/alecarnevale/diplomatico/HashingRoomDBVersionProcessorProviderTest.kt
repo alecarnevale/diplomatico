b21359a153ffabe0c9056ae5303ef25da5b922de
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2024-10-11 18:03:06 +0200

    add a test to check report changes when content for entity changes

--- processors/src/test/kotlin/com/alecarnevale/diplomatico/HashingRoomDBVersionProcessorProviderTest.kt
+++ processors/src/test/kotlin/com/alecarnevale/diplomatico/HashingRoomDBVersionProcessorProviderTest.kt
@@ -219,6 +219,81 @@ class HashingRoomDBVersionProcessorProviderTest {
     )
   }
 
+  @Test
+  fun `GIVEN a class Foo with Database annotation and entities field WHEN @HashingRoomDBVersion is applied to Foo but Foo changes, THEN reports generated change`() {
+    var fooEntity =
+      SourceFile.kotlin(
+        "FooEntity.kt",
+        """
+        package com.example
+
+        import androidx.room.Entity
+        
+        @Entity
+        data class FooEntity(
+          val x: Int,
+        )
+        """.trimIndent(),
+      )
+
+    val fooDatabase =
+      SourceFile.kotlin(
+        "FooDatabase.kt",
+        """
+        package com.example
+
+        import androidx.room.Database
+        import com.alecarnevale.diplomatico.api.HashingRoomDBVersion
+        
+        @HashingRoomDBVersion
+        @Database(entities = [FooEntity::class])
+        class FooDatabase
+        """.trimIndent(),
+      )
+
+    var result = compileSourceFiles(fooEntity, fooDatabase)
+
+    assertEquals(KotlinCompilation.ExitCode.OK, result.result.exitCode)
+
+    result.assertGeneratedResources("com/alecarnevale/diplomatico/results/report.csv")
+    result.assertGeneratedContent(
+      "com/alecarnevale/diplomatico/results/report.csv",
+      """
+      com.example.FooDatabase,rH6hy7aLdVv1fxCZlPqtsqRZauGUsJTim0CxRMDo8vg=
+      
+      """,
+    )
+
+    fooEntity =
+      SourceFile.kotlin(
+        "FooEntity.kt",
+        """
+        package com.example
+
+        import androidx.room.Entity
+        
+        @Entity
+        data class FooEntity(
+          val x: Int,
+          val y: String,
+        )
+        """.trimIndent(),
+      )
+
+    result = compileSourceFiles(fooEntity, fooDatabase)
+
+    assertEquals(KotlinCompilation.ExitCode.OK, result.result.exitCode)
+
+    result.assertGeneratedResources("com/alecarnevale/diplomatico/results/report.csv")
+    result.assertGeneratedContent(
+      "com/alecarnevale/diplomatico/results/report.csv",
+      """
+      com.example.FooDatabase,NyNDpYWpN9vFEOcaDrAI7JLXXA6MgaIKN1TLQQ7m/ms=
+      
+      """,
+    )
+  }
+
   private fun compileSourceFiles(vararg sourceFiles: SourceFile): KspCompilationResult {
     val kotlinCompilation =
       KotlinCompilation().apply {
