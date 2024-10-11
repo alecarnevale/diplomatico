aac0a0bae9455f423c405d74d39a7cfc11afcdf1
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2024-10-11 19:38:37 +0200

    add new entity

--- demo/app/src/main/java/com/alecarnevale/diplomatico/demo/DrinkDatabase.kt
+++ demo/app/src/main/java/com/alecarnevale/diplomatico/demo/DrinkDatabase.kt
@@ -4,17 +4,28 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.alecarnevale.diplomatico.api.HashingRoomDBVersion
+import com.alecarnevale.diplomatico.demo.cocktail.Cocktail
+import com.alecarnevale.diplomatico.demo.cocktail.CocktailDao
+import com.alecarnevale.diplomatico.demo.cocktail.converters.BaseSpiritConverter
+import com.alecarnevale.diplomatico.demo.cocktail.converters.DistilledConverter
 import com.alecarnevale.diplomatico.demo.drink.Drink
 import com.alecarnevale.diplomatico.demo.drink.DrinkDao
 
 @HashingRoomDBVersion
 @Database(
-  entities = [Drink::class],
+  entities = [Drink::class, Cocktail::class],
   version = 1,
+)
+@TypeConverters(
+  BaseSpiritConverter::class,
+  DistilledConverter::class,
 )
 internal abstract class DrinkDatabase : RoomDatabase() {
   abstract fun drinkDao(): DrinkDao
+
+  abstract fun cocktailDao(): CocktailDao
 }
 
 internal object DrinkDatabaseProvider {
