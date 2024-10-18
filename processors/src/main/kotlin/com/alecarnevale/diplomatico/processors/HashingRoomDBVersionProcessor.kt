36e22308194bd84ecc26036a9b4e9acea07c1717
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2024-10-18 07:50:40 +0200

    extract contributesEntitiesForDatabase

--- processors/src/main/kotlin/com/alecarnevale/diplomatico/processors/HashingRoomDBVersionProcessor.kt
+++ processors/src/main/kotlin/com/alecarnevale/diplomatico/processors/HashingRoomDBVersionProcessor.kt
@@ -1,6 +1,8 @@
 package com.alecarnevale.diplomatico.processors
 
+import com.alecarnevale.diplomatico.api.ContributesRoomDBVersion
 import com.alecarnevale.diplomatico.api.HashingRoomDBVersion
+import com.alecarnevale.diplomatico.visitor.ContributesRoomDBVersionVisitor
 import com.alecarnevale.diplomatico.visitor.HashingOutput
 import com.alecarnevale.diplomatico.visitor.HashingRoomDBVersionVisitor
 import com.google.devtools.ksp.containingFile
@@ -25,30 +27,48 @@ internal class HashingRoomDBVersionProcessor(
   private var databaseResolvedSymbols = setOf<KSAnnotated>()
 
   override fun process(resolver: Resolver): List<KSAnnotated> {
-    val visitor = HashingRoomDBVersionVisitor(resolver = resolver, logger = logger)
+    val databasesVisitor = HashingRoomDBVersionVisitor(resolver = resolver, logger = logger)
+    val contributesVisitor = ContributesRoomDBVersionVisitor(resolver = resolver, logger = logger)
 
     val annotationNameForDatabase = HashingRoomDBVersion::class.qualifiedName ?: return emptyList()
 
     databaseResolvedSymbols = resolver.getSymbolsWithAnnotation(annotationNameForDatabase).toSet()
+    // TODO add validation phase, so casting to KSClassDeclaration will be safer
 
     if (databaseResolvedSymbols.isEmpty()) {
       return emptyList()
     }
 
+    val annotationNameForEntities = ContributesRoomDBVersion::class.qualifiedName ?: return emptyList()
+
+    // gather all entities that are contributing for a database
+    val contributesEntities = resolver.getSymbolsWithAnnotation(annotationNameForEntities).toSet()
+    // TODO add validation phase, so we can convert List<KSAnnotated> as List<KSClassDeclaration>
+    val contributesEntitiesForDatabase: Map<KSClassDeclaration?, List<KSAnnotated>> =
+      contributesEntities.groupBy { entity ->
+        // grouping by database class extracted from the argument ContributesRoomDBVersion::roomDB
+        entity.accept(contributesVisitor, Unit)
+      }
+
     // retrieve the set of entities (with their transitive) for each Room database annotated with HashingRoomDBVersion
     val entitiesForDatabase =
-      mutableMapOf<KSAnnotated, Set<KSClassDeclaration>>().apply {
+      mutableMapOf<KSClassDeclaration, Set<KSClassDeclaration>>().apply {
         databaseResolvedSymbols.forEach { roomDB ->
-          roomDB.accept(visitor, Unit)?.let {
+          // TODO this cast maybe temporary, until validation phase is added
+          roomDB as KSClassDeclaration
+          roomDB.accept(databasesVisitor, Unit)?.let {
             this[roomDB] = it
           }
         }
       }
 
+    // for each database, merge their entities with the contributes already discovered
+    // TODO
+
     // generate an Output for each database, computing hash value from the set of entities found in the previous step
     val hashingOutput = HashingOutput(resolver, logger)
     entitiesForDatabase.entries.forEach {
-      hashingOutput.generate(it.key as KSClassDeclaration, it.value)?.let { output ->
+      hashingOutput.generate(it.key, it.value)?.let { output ->
         outputs.add(output)
       }
     }
---
+++ processors/src/main/kotlin/com/alecarnevale/diplomatico/visitor/ContributesRoomDBVersionVisitor.kt
@@ -0,0 +1,40 @@
+package com.alecarnevale.diplomatico.visitor
+
+import com.alecarnevale.diplomatico.api.ContributesRoomDBVersion
+import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.processing.Resolver
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSNode
+import com.google.devtools.ksp.symbol.KSType
+import com.google.devtools.ksp.visitor.KSEmptyVisitor
+
+/**
+ * Returns the database class declaration defined by applying [ContributesRoomDBVersion] with the [ContributesRoomDBVersion.roomDB] specified.
+ */
+internal class ContributesRoomDBVersionVisitor(
+  private val resolver: Resolver,
+  private val logger: KSPLogger,
+) : KSEmptyVisitor<Unit, KSClassDeclaration?>() {
+  override fun visitClassDeclaration(
+    classDeclaration: KSClassDeclaration,
+    data: Unit,
+  ): KSClassDeclaration? =
+    // find ContributesRoomDBVersion annotation
+    classDeclaration.annotations.firstOrNull { it.shortName.getShortName() == ContributesRoomDBVersion::class.simpleName }?.let { annotation ->
+      // find ContributesRoomDBVersion.roomDB argument
+      (annotation.arguments.firstOrNull { it.name?.getShortName() == ContributesRoomDBVersion::roomDB.name }?.value as? KSType)?.let { argument ->
+        // find KSClassDeclaration for the specified argument value
+        argument.declaration.qualifiedName?.let {
+          resolver.getClassDeclarationByName(it)
+        } ?: run {
+          logger.error("No ContributesRoomDBVersion::roomDB found while visiting $classDeclaration")
+          null
+        }
+      }
+    }
+
+  override fun defaultHandler(
+    node: KSNode,
+    data: Unit,
+  ): KSClassDeclaration? = null
+}
