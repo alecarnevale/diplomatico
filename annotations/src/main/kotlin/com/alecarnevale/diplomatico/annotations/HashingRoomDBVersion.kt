7c31d5e81b15cd533791e10a8186122b97dc9a29
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2024-11-13 18:15:04 +0100

    add HashingRoomDBVersion.contributes property

--- annotations/src/main/kotlin/com/alecarnevale/diplomatico/annotations/HashingRoomDBVersion.kt
+++ annotations/src/main/kotlin/com/alecarnevale/diplomatico/annotations/HashingRoomDBVersion.kt
@@ -1,9 +1,15 @@
 package com.alecarnevale.diplomatico.annotations
 
+import kotlin.reflect.KClass
+
 /**
  * When annotates a Room database, a KSP processor will generate an hash for it based on the entities stored.
  * Hashes for each annotated databases are listed in the same report.
+ *
+ * @param contributes works in the same way of [ContributesRoomDBVersion] annotation, any referenced class will be used as input for the hashing function.
  */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
-annotation class HashingRoomDBVersion
+annotation class HashingRoomDBVersion(
+  val contributes: Array<KClass<*>> = [],
+)
